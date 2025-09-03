cc18b20706182419f10c9ddafb3505b2e6ac676a
Saamir <saamirkhan7513@gmail.com>
2025-09-03 18:53:33 +0530

    changes

--- inngest/client.js
+++ inngest/client.js
@@ -2,5 +2,5 @@ import {Inngest} from "inngest";
 
 export const inngest = new Inngest({id: "ticketing-system",
       eventKey: process.env.INNGEST_EVENT_KEY,     
-  signingKey: process.env.INNGEST_SIGNING_KEY,
+  
 })
