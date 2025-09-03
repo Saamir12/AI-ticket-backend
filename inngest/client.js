35ccd5e8ebfed48c289dcc1eb5d78076fd8c34e3
Saamir <saamirkhan7513@gmail.com>
2025-09-03 18:02:36 +0530

    final

--- inngest/client.js
+++ inngest/client.js
@@ -1,5 +1,6 @@
 import {Inngest} from "inngest";
 
 export const inngest = new Inngest({id: "ticketing-system",
-    apiKey: process.env.INNGEST_API_KEY
+      eventKey: process.env.INNGEST_EVENT_KEY,     
+  signingKey: process.env.INNGEST_SIGNING_KEY,
 })
